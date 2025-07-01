7c23bb4c30e1658919fa5d9934a684883c811176
Anup kumar <anupk7196@gmail.com>
2025-07-02 04:12:14 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import { useState } from 'react'
-import './App.css'
-
+import Body from "./components/Body";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div className=" text-3xl text-blue-400 text-bold">Welcome to NetflixGPT</div>
-    </>
+    <Body/>
   )
 }
 
